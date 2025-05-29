0167ac60236e842ba3cd6c68988c8af511f921d2
Zach Simpson <Zach33Simpson@gmail.com>
2025-05-29 16:16:52 -0400

    Added ability to hug penguins

--- Penguin.py
+++ Penguin.py
@@ -17,14 +17,14 @@ class Penguin:
         self.exp = 0
 
         #increase stats depending on attribute
-        if self.attribute is 'damage':
+        if self.attribute == 'damage':
             self.damage+=1
-        elif self.attribute is 'defence':
+        elif self.attribute == 'defence':
             self.defence+=1
         else :
             self.speed+=1
     
-    # returns the penguins stats as a string
+    # Returns the penguins stats as a string
     def displayStats(self) :
         out = f'Level: {self.level}\nExp: {self.exp}/{self.level**2}\nHealth: {self.health}\n'
         out += f'Damage: {self.damage}\nDefence: {self.defence}\nSpeed: {self.speed}\n'
@@ -32,13 +32,27 @@ class Penguin:
         out += f'Attribute: {self.attribute.capitalize()}'
         return out
     
-    # pet the penguin to increase affection by 1
+    # Pet the penguin to increase affection by 1
     def pet(self) :
         self.affection+=1
         if self.affection == 10 :
+            return f'✨ 🐧 You pet your penguin 🐧 ✨ (Affection + 1 (MAX): {self.affection}/10)'
+        elif self.affection >= 11 :
             self.affection = 10
-            return f'✨ 🐧 Your penguin has been pet 🐧 ✨ (Affection +1 (MAX): {self.affection}/10)'
-        elif self.affection > 10 :
-            return f'✨ 🐧 Your penguin has been pet 🐧 ✨ (Affection +0 (MAX): {self.affection}/10)'
-        return f'✨ 🐧 Your penguin has been pet 🐧 ✨ (Affection +1 : {self.affection}/10)'
-        
+            return f'✨ 🐧 You pet your penguin 🐧 ✨ (Affection + 0 (MAX): {self.affection}/10)'
+        return f'✨ 🐧 You pet your penguin 🐧 ✨ (Affection + 1 : {self.affection}/10)'
+    
+    # Hug your penguin to increase affection by 2
+    def hug(self) :
+        self.affection+=2
+        if self.affection < 5:
+            return f'Your penguin backs away (Affection too low: {self.affection}/10)'
+        elif self.affection == 10 :
+            return f'✨ 🐧 You hug your penguin 🐧 ✨ (Affection + 2 (MAX): {self.affection}/10)'
+        elif self.affection == 11 :
+            self.affection = 10
+            return f'✨ 🐧 You hug your penguin 🐧 ✨ (Affection + 1 (MAX): {self.affection}/10)'
+        elif self.affection >= 12 :
+            self.affection = 10
+            return f'✨ 🐧 You hug your penguin 🐧 ✨ (Affection + 0 (MAX): {self.affection}/10)'
+        return f'✨ 🐧 Your penguin has been pet 🐧 ✨ (Affection + 2 : {self.affection}/10)'
